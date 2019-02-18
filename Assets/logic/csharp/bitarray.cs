1f8533b6befb76a9dc2ae15b5544a94eea1efceb
Archie Sullivan <as8756m@greenwich.ac.uk>
2019-02-18 17:17:24 +0000

    Added bitarray.cs

---
+++ Assets/logic/csharp/bitarray.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Bitarray
+{
+
+    /**
+     * 
+     * 
+     * 
+     */
+    public class Bitarray : BitFlag
+    {
+
+
+        uint max;
+        /// <summary>The array of bits to operate on, use <c>&=</c> with a set of numbers to use it</summary>
+        public ushort narray;
+        private bool isPastMax(uint num)
+        {
+            return num < ushort.MaxValue;
+        }
+        Bitarray(uint max_, ushort narray_)
+        {
+            if (isPastMax(max_))
+            {
+                throw new System.IndexOutOfRangeException();
+            }
+            else
+            {
+                max = max_;
+                narray |= narray_;
+            }
+        }
+        /**
+         * <param name="i">The index of the bit array to access</param>
+         * <returns>the boolean value at the position specified by the index</returns>
+         */
+        public bool get(ushort i)
+        {
+            if (isPastMax(i))
+            {
+                throw new System.IndexOutOfRangeException();
+            }
+            else
+            {
+                return System.Convert.ToBoolean(narray & (1 << i));
+            }
+        }
+        /**
+         * <param name="i">The index of the bit to toggle</param>
+         * <summary></summary>
+         */
+        public void toggle(ushort i)
+        {
+            if (isPastMax(i))
+            {
+                throw new System.IndexOutOfRangeException();
+            }
+            else
+            {
+                narray ^= (2 << i);
+            }
+        }
+    }
+}
