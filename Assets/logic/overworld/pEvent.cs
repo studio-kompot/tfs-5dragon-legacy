97d1ac4d6fd7d076851fc453a04ee75f41432ebb
Archie Sullivan <as8756m@greenwich.ac.uk>
2019-03-01 15:05:22 +0000

    Beginning on the event system...

---
+++ Assets/logic/overworld/pEvent.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pEvent : MonoBehaviour {
+    ///<summary>Initates "Pass mode", meaning the player can pass onto the object and activate it from there.</summary>
+    public bool PassMode;
+    // Use this for initialization
+    void Start () {
+        GetComponent<BoxCollider>().isTrigger = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    Collider OnTriggerEnter(Collider c) { }
+}
