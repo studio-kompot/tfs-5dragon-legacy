caceda668e75521efbf308527eef51980eeaa7e9
Archie Sullivan <as8756m@greenwich.ac.uk>
2019-03-14 23:14:43 +0000

    Initial progress on a dialog tree system

--- Assets/logic/overworld/Dialog/DialogManager.cs
+++ Assets/logic/overworld/Dialog/DialogManager.cs
@@ -5,25 +5,32 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class DialogManager : MonoBehaviour {
+#region Variables
     public static DialogManager instance;
-    public void Awake() {
-        if (instance != null) Debug.LogWarning("Error: DialogManager object already instanced");
-        else instance = this;
-    }
-
     public GameObject DialogPanel;
     public Text DName;
     public Text DText;
     public Image DFaceplate;
     public Queue<DialogBase.DFrame> dq;
     public float delay = 0.001f;
+    private bool isOptionsType;
+    private bool inDialog; //bugfix to garbled text appearing
+    public GameObject OptionsContainer;
+#endregion
 #region Methods
     void Start() {
         dq = new Queue<DialogBase.DFrame>();
     }
+    public void Awake() {
+        if (instance != null) Debug.LogWarning("Error: DialogManager object already instanced");
+        else instance = this;
+    }
 
     public void Init(DialogBase d) {
         dq.Clear();
+        if (inDialog) return;
+        inDialog = true;
+        isOptionsType = (d is DialogOptions);
         DialogPanel.SetActive(true);
         foreach (DialogBase.DFrame i in d.DialogData) {
             dq.Enqueue(i);
@@ -33,6 +40,8 @@ public class DialogManager : MonoBehaviour {
     public void Send() {
         if (dq.Count == 0) {
             DialogPanel.SetActive(false);
+            OptionsContainer.setActive(isOptionsType);
+            inDialog = DialogPanel.activeInHierarchy;
             return;
         } else {
             DialogBase.DFrame current = dq.Dequeue();
---
+++ Assets/logic/overworld/Dialog/DialogOptions.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewDialogOptions", menuName = "Dialog Option")]
+public class DialogOptions : DialogBase {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
