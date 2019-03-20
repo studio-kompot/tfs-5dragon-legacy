3b1be1900032707ccdea6e9e59916288c236e23b
Danny O'Dwyer <ixorxolouxlll8o@gmail.com>
2019-03-20 18:39:06 +0000

    Small Update to fix Anand's Update.
    The Key item can now be collected and can be used to destroy the door when collected.

--- Assets/logic/overworld/Door.cs
+++ Assets/logic/overworld/Door.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Door : MonoBehaviour
 {
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
 
 
