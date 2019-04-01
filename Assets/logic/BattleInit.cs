e0049b0e730c29a8b78ef52c1750b11599ac618d
Atlas <AtlasJan@gmx.com>
2019-04-01 01:04:31 +0100

    Inital github commit

--- Assets/logic/BattleInit.cs
+++ Assets/logic/BattleInit.cs
@@ -6,4 +6,17 @@ public static class BattleInit {
     //TODO: MOAR CODE!!!!!!
 
     public static List<Enemy> Enemies;
+    public enum BBackground {
+      field,
+      cave,
+      volcano
+    }
+    public static BBackground ABackground;
+    public enum MMusic {
+      standard,
+      miniboss,
+      boss,
+      special
+    }
+    public static MMusic AMusic;
 }
--- Assets/logic/battle/Background.cs
+++ Assets/logic/battle/Background.cs
@@ -13,9 +13,4 @@ public class Background : MonoBehaviour {
         current = GetComponent<Image>();
         current.sprite = Backgrounds[(int)BattleInit.ABackground];
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 }
